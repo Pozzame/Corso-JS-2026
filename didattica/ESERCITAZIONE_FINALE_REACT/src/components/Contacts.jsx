413a99ef0ebf2e9b80a450ab1ff8ac7f1731fdfe
Pozzame <pozzame@gmail.com>
2026-09-19 14:29:16 +0200

    a

--- didattica/ESERCITAZIONE_FINALE_REACT/src/components/Contacts.jsx
+++ didattica/ESERCITAZIONE_FINALE_REACT/src/components/Contacts.jsx
@@ -7,7 +7,7 @@ function Contacts(){
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2970.093435450764!2d12.48689581156884!3d41.89084761449602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f61b6532013ad%3A0x28f1c82e908503c4!2sColosseo!5e0!3m2!1sit!2sit!4v1789812150191!5m2!1sit!2sit" 
                     width="400" 
                     height="400" 
-                    style="border:0;" 
+                    style={{border:0}}
                     allowfullscreen="" 
                     loading="lazy" 
                     referrerpolicy="strict-origin-when-cross-origin">
